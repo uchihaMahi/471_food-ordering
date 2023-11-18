050c7ee4185251fc447aa99b5ae507af83f1e285
Mahdi Hossain <98962528+MahdiHossain@users.noreply.github.com>
2023-11-18 23:42:18 +0600

    auth update in mahdi

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const authController = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
+const {verifyToken, verifyTokenAdmin} = require('../middlewares/verifyToken')
 
 // register
 authController.post('/register', async(req, res) => {
@@ -10,8 +11,9 @@ authController.post('/register', async(req, res) => {
       if(isExisting){
         throw new Error("Already such an account with this email. Try a new one!")
       }
-
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      
+      const salt = await bcrypt.genSalt(10)
+      let hashedPassword = await bcrypt.hash(req.body.password, salt)
 
       const newUser = await User.create({...req.body, password: hashedPassword})
       const {password, ...others} = newUser._doc
@@ -22,6 +24,8 @@ authController.post('/register', async(req, res) => {
         return res.status(500).json(error.message)
     }
 })
+
+
 
 // login
 authController.post('/login', async(req, res) => {
@@ -37,7 +41,60 @@ authController.post('/login', async(req, res) => {
         throw new Error("User credentials are wrong!")
        }
 
-       const {password, ...others} = user
+       const {password, ...others} = user._doc
+       const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
+
+       return res.status(200).json({others, token})
+    } catch (error) {
+        return res.status(500).json(error.message) 
+    }
+})
+
+
+module.exports = authControllerconst authController = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
+const {verifyToken, verifyTokenAdmin} = require('../middlewares/verifyToken')
+
+// register
+authController.post('/register', async(req, res) => {
+    try {
+      const isExisting = await User.findOne({email: req.body.email})  
+      if(isExisting){
+        throw new Error("Already such an account with this email. Try a new one!")
+      }
+      
+      const salt = await bcrypt.genSalt(10)
+      let hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+      const newUser = await User.create({...req.body, password: hashedPassword})
+      const {password, ...others} = newUser._doc
+      const token = jwt.sign({id: newUser._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
+
+      return res.status(201).json({others, token})
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+})
+
+
+
+// login
+authController.post('/login', async(req, res) => {
+    try {
+       const user = await User.findOne({email: req.body.email}) 
+       if(!user){
+          throw new Error("User credentials are wrong!")
+       }
+
+       // 123456, [lkoiuytfdrse5rd6tfgyhijopk[l;]'\[pkojiugyftdrzsdxtfycghu]]
+       const comparePass = await bcrypt.compare(req.body.password, user.password)
+       if(!comparePass){
+        throw new Error("User credentials are wrong!")
+       }
+
+       const {password, ...others} = user._doc
        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
 
        return res.status(200).json({others, token})

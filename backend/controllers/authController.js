4a3545d19b515bc8e5ad30c0c763335fb8a75467
uchihaMahi <148608654+uchihaMahi@users.noreply.github.com>
2023-11-18 23:33:29 +0600

    authUpdate

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,15 +11,18 @@ authController.post('/register', async(req, res) => {
       if(isExisting){
         throw new Error("Already such an account with this email. Try a new one!")
       }
+      else{
+        const salt = await bcrypt.genSalt(10)
+        let hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+        const newUser = await User.create({...req.body, password: hashedPassword})
+        const {password, ...others} = newUser._doc
+        const token = jwt.sign({id: newUser._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
+
+        return res.status(201).json({others, token})
+
+      }
       
-      const salt = await bcrypt.genSalt(10)
-      let hashedPassword = await bcrypt.hash(req.body.password, salt)
-
-      const newUser = await User.create({...req.body, password: hashedPassword})
-      const {password, ...others} = newUser._doc
-      const token = jwt.sign({id: newUser._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
-
-      return res.status(201).json({others, token})
     } catch (error) {
         return res.status(500).json(error.message)
     }
@@ -40,11 +43,12 @@ authController.post('/login', async(req, res) => {
        if(!comparePass){
         throw new Error("User credentials are wrong!")
        }
+       if(user && comparePass){
+        const {password, ...others} = user
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
 
-       const {password, ...others} = user
-       const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: '5h'})
-
-       return res.status(200).json({others, token})
+        return res.status(200).json({others, token})
+      }
     } catch (error) {
         return res.status(500).json(error.message) 
     }
